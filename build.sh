700a7371e3c2c3327292e8772f62db70efb628f4
md.irohas <md.irohas@gmail.com>
2019-02-10 12:37:07 +0900

    Update Circle-CI configuration.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-GOOS=darwin GOARCH=amd64 go build -o geoipcli-go-macos-amd64
-GOOS=linux GOARCH=amd64 go build -o geoipcli-go-linux-amd64
+GOOS=darwin GOARCH=amd64 go build -o build/geoipcli-go-macos-amd64
+GOOS=linux GOARCH=amd64 go build -o build/geoipcli-go-linux-amd64

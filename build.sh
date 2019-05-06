8402367a76a897873f60c496410f9bad7916f171
md.irohas <md.irohas@gmail.com>
2019-05-06 20:31:58 +0900

    Add compilation for windows.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+GOOS=windows GOARCH=amd64 go build -o build/geoipcli-go-windows-amd64
 GOOS=darwin GOARCH=amd64 go build -o build/geoipcli-go-macos-amd64
 GOOS=linux GOARCH=amd64 go build -o build/geoipcli-go-linux-amd64
